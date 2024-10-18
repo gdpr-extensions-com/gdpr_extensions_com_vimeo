fff914d43cb2f8c801ac0b97335430a8acdceeba
gdpr-extensions-com <support@gdpr-extensions.com>
2024-10-18 10:47:26 +0500

    Prepare for stable release 1.0.10

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'support@gdpr-extensions.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.9',
+    'version' => '1.0.10',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.10',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
